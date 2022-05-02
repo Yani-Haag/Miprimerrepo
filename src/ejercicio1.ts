64aa028ad5201daa7abb7adc5c00b703d9244235
Yani-Haag <104107528+Yani-Haag@users.noreply.github.com>
2022-05-02 16:24:42 -0300

    ejercicio1clasekaren hice codigo

---
+++ src/ejercicio1.ts
@@ -0,0 +1,12 @@
+console.log("Crear un posteo en facebook");
+
+console.log("Paso 1");
+console.log("Paso 2");
+console.log("Paso 3");
+console.log("Paso 4");
+console.log("Paso 5");
+console.log("Paso 6");
+console.log("Paso 7");
+console.log("Paso 8");
+console.log("Paso 9");
+console.log("Paso 10");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("mi primer comit, es magico");
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let dato3 = document.getElementById("dato3");
+
+let btnEnviar = document.getElementById("enviar");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+let rotulo3 = document.getElementById("rotulo3");
+
+ rotulo1.innerHTML = "numero1"
+ rotulo2.innerHTML = "numero2"
+ rotulo3.innerHTML = "numero3"
+
+ let numero1: number = number(dato1.value);
+ let numero2: number = number(dato2.value);
+ let numero3: number = number(dato3.value);
+
+ btnEnviar.addEventListener("click", () => {
+ 
+ if (numero1 > numero2 && numero1 > numero3) {
+       console.log("El numero" + numero1 + "es el mayor");
+
+ } else if(numero2 > numero1 && numero2 > numero3) {
+       console.log("El numero" + numero2 + "es el mayor");
+ 
+ } else if(numero3 > numero1 && numero3 > numero2) {
+       console.log("El numero" + numero3 + "es el mayor");
+ } 
